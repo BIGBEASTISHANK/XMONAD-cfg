6d8b6b622f9538d975e7453fc3ff4ad809bd13c1
BIGBEASTISHANK <77890275+BIGBEASTISHANK@users.noreply.github.com>
2021-10-23 09:58:30 +0530

    Update package-install.sh

--- package-install.sh
+++ package-install.sh
@@ -1,1 +1,1 @@
-exec sudo pacman -Syu nano xorg xmonad xmonad-contrib xmobar dmenu nitrogen opera opera-ffmpeg-codecs pcmanfm
+exec sudo pacman -Syu nano xorg xmonad xmonad-contrib xmobar dmenu nitrogen opera opera-ffmpeg-codecs pcmanfm lxappearance
